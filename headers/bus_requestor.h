45d5b5d89c4c434b77f64fe2d759d0fa76af5dba
nadav malachi <nadav.malachi@gmail.com>
2025-01-11 21:04:58 +0200

    filled requestor

---
+++ headers/bus_requestor.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include "defines.h"
+
+// Definition of the BusRequestor struct
+typedef struct {
+    int operation;               // Operation type (e.g., read, write, invalidate)
+    int address;                 // Address associated with the bus request
+    int start_cycle;             // Cycle when the request started
+    bool busRequestorOccupied_now;    // True if a bus request is waiting to be sent or in progress
+    bool busRequestorOccupied_updated; // True if the requestor's status was recently updated
+    bool busRequestInTransaction_now;  // True if a bus request was already sent, waiting to end
+    int priority;                     // Priority of the bus requestor
+    int id;                           // Identifier for the BusRequestor
+} BusRequestor;
+
+// Function to create and initialize a BusRequestor
+BusRequestor* bus_requestor_create(int id);
+
+// Funcion to destroy requestor memoey allck
+void bus_requestor_destroy(BusRequestor* requestor);
+
+// Function to check if the BusRequestor is already occupied
+bool BusRequestorAlreadyOccupied(const BusRequestor* requestor);
+
+// Function to reset the BusRequestor to its initial state
+void bus_requestor_reset(BusRequestor* requestor);
+
+// Function to request an action from the bus (sets the request operation and address)
+void RequestActionFromBus(BusRequestor* requestor, int addr, int BusActionType);
+
+// Function to perform the bus operation if granted
+//void DoOperation(BusRequestor* requestor, BusManager* manager, int cycle);
